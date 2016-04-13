c9c5ba1b020a390d4f02ce4c269e918c67749ce5
Guillaume Allegret <gui.allegret@gmail.com>
2016-04-13 20:33:32 +0200

    Correct Test Case

--- src/Illuminate/Support/Facades/Auth.php
+++ src/Illuminate/Support/Facades/Auth.php
@@ -4,18 +4,6 @@ namespace Illuminate\Support\Facades;
 
 /**
  * @see \Illuminate\Auth\AuthManager
- * @method static \Illuminate\Contracts\Auth\Guard guard($name = null)
- * @method static \Illuminate\Auth\SessionGuard createSessionDriver($name, $config)
- * @method static \Illuminate\Auth\TokenGuard createTokenDriver($name, $config)
- * @method static string getDefaultDriver()
- * @method static void shouldUse($name)
- * @method static void setDefaultDriver($name)
- * @method static viaRequest($driver, callable $callback)
- * @method static \Closure userResolver()
- * @method static resolveUsersUsing(\Closure $userResolver)
- * @method static extend($driver, \Closure $callback)
- * @method static provider($name, \Closure $callback)
- * @method static \Illuminate\Contracts\Auth\UserProvider createUserProvider($provider)
  * @see \Illuminate\Contracts\Auth\Factory
  * @see \Illuminate\Contracts\Auth\Guard
  * @see \Illuminate\Contracts\Auth\StatefulGuard
